733e22214d03b0ed122ce87edaabee32c05ba724
matheuspm05 <159352318+matheuspm05@users.noreply.github.com>
2026-02-25 10:39:54 -0300

    Add function to find largest element and refactor main
    Refactor main function to include multiplication prompt and result display. Add function to find the largest element in an array.

--- ED2/rec1.c
+++ ED2/rec1.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
+#define TAMANHO 5
+
 
 int multiplica(int a, int b) {
-    
     if (b == 0) {
         return 0;
     }
     return a + multiplica(a, b - 1);
 }
 
+int maiorElemento(int *v, int tam) {
+    if (tam == 1) {
+        return v[0];
+    }
+
+    int maior = maiorElemento(v, tam - 1);
+
+    if (v[tam - 1] > maior) {
+        return v[tam - 1];
+    } else {
+        return maior;
+    }
+}
+
 int main() {
     int a, b;
-
-    printf("Digite dois inteiros positivos: ");
-    scanf("%d %d", &a, &b);
+    int vetor[TAMANHO] = {1, 2, 3, 20, 5};
 
     
-   printf("Resultado: %d\n", multiplica(a, b));
-  
+    printf("Digite dois inteiros positivos para multiplicar: ");
+    scanf("%d %d", &a, &b);
+    printf("Resultado da multiplicacao: %d\n", multiplica(a, b));
+
+    
+    printf("Maior elemento do vetor: %d\n", maiorElemento(vetor, TAMANHO));
 
     return 0;
 }
